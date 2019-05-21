cd516d19c4446400b68fcb299fd827655e784cd1
[your <leigh.tm@gmail.com>
2019-05-21 13:01:10 -0400

    stated over but having issues trying to render

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import { UserForm } from './components/UserForm';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import index from './index';
+import PrivateRoute from './PrivateRoute';
+import Login from './mainLogOnPage/components/Login';
+import PostMessage from './mainLogOnPage/components/PostMessage'
 
 
-
-
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <UserForm  />
-            </div>
-        );
-    }
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/PostMessage">Admin Access</Link>
+          </li>
+        </ul>
+        <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/PostMessage" component={PostMessage} />
+      </div>
+    </Router>
+  );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 import App from './App';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider }  from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from '../src/mainLogOnPage/reducers';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
+React.DOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
+
 
