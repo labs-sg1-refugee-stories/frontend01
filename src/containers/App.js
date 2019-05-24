f559cd36ab89bbc99760dca9d482b0c6ec9eeaab
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-05-24 10:22:32 -0700

    PrivateRoute fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
-import MessageBoardPage from './MessageBoard.Page';
-import { 
-  BrowserRouter as Router, 
-  Route 
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import '../App.css';
-import AdminLogin from './AdminLogin';
+import React, { Component } from 'react'
+import MessageBoardPage from './MessageBoard.Page'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+import Home from '../components/Home'
+import '../App.css'
+import AdminLogin from './AdminLogin'
 
-import AdminContainer from './AdminContainer';
-import PrivateRoute from '../privateRoute';
-
-
+import AdminContainer from './AdminContainer'
+import PrivateRoute from '../PrivateRoute'
 
 class App extends Component {
   render() {
@@ -20,14 +15,18 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
-          <Route exact path="/" component={Home} />
-          <Route path='/messages' component={MessageBoardPage}/>
-          <Route path='/AdminLogin' component={AdminLogin}/>
-          <PrivateRoute exact path="/AdminContainer" component={AdminContainer} />
-          </div>
+          <Route exact path='/' component={Home} />
+          <Route path='/messages' component={MessageBoardPage} />
+          <Route path='/AdminLogin' component={AdminLogin} />
+          <PrivateRoute
+            exact
+            path='/AdminContainer'
+            component={AdminContainer}
+          />
+        </div>
       </Router>
-    );
+    )
   }
 }
 
-export default App;
+export default App
